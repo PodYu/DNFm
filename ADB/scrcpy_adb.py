59a4e7f0f497228b1596afa4456c0f0099d49b61
PodYu <yuyangtju1123@gmail.com>
2024-08-18 00:02:13 +0800

    refactor(main.py): 更新scrcpy_adb模块导入路径
    更改main.py中scrcpy_adb模块的导入方式，从直接导入更改为从ADB包中导入。这解决了直接引用模块产生的依赖问题，提高了代码的可维护性。

--- ADB/scrcpy_adb.py
+++ ADB/scrcpy_adb.py
@@ -13,14 +13,15 @@ def handle_frame(frame):
     cv2.waitKey(10)
     return frame
 
-def click(x, y):
+def click(x: int, y: int):
     """
     Clicks at the specified coordinates on the screen.
 
     :param x: The x-coordinate of the click location.
     :param y: The y-coordinate of the click location.
     """
-    client.device.shell(f'input tap {x} {y}')
+    #client.device.shell(f'input tap {x} {y}')
+    client.control.touch(x, y, action=const.ACTION_DOWN, touch_id=-1)
 
 
 def drag(start_x, start_y, end_x, end_y, duration=500):
@@ -38,8 +39,8 @@ def drag(start_x, start_y, end_x, end_y, duration=500):
 
 # 如果你已经知道设备的序列号或 IP 地址
 client = scrcpy.Client(
-    #device="192.168.8.4:5555",#天选
-    device="192.168.3.43:5555",#sweet
+    device="192.168.8.4:5555",#天选
+    #device="192.168.3.43:5555",#sweet
     max_width=1000,
     bitrate=8000000,
     max_fps=30,
@@ -52,4 +53,4 @@ client.add_listener(scrcpy.EVENT_FRAME, handle_frame)
 client.start(threaded=True)
 
 # 示例：模拟点击屏幕
-# click(100, 200)  # 在 x=100, y=200 的位置点击
+click(100, 200)  # 在 x=100, y=200 的位置点击
